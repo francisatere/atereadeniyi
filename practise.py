43ea333f5188c50ef7848d8c35d03c77c59d3f1e
Francis Atere <francisatereniyi@gmail.com>
2021-11-26 18:48:51 +0000

    blablabla

---
+++ practise.py
@@ -0,0 +1,16 @@
+#francis = "lagos boy"
+#francis = francis.upper()
+#print(francis)
+
+#francis = "LAGOS BOY"
+#francis = francis.casefold()
+#print(francis)
+
+#francis = "lagos boy"
+#francis = francis.capitalise()
+#print(francis)
+
+francis = "lagos boy"
+francis = francis.join()
+print(francis)
+
---
+++ string methods.py
@@ -0,0 +1,37 @@
+#francis = "lagos boy"
+#francis = francis.upper()
+#print(francis)
+
+#francis = "LAGOS BOY"
+#francis = francis.casefold()
+#print(francis)
+
+#francis = "lagos boy is meticulous"
+#francis = francis.capitalize()
+#print(francis)
+
+#francis = "guava is the sweetest when ripe among fruits"
+#francis = francis.count()
+#print(francis)
+
+#francis = "guava is the sweetest among the fruits"
+#francis = francis.isalpha()
+#print(francis)
+
+#francis = "guava is the sweetest when ripe among fruits"
+#francis = francis.isnumeric()
+#print(francis)
+
+francis = "132547698090"
+francis = francis.isdigit()
+print(francis)
+
+adeniyi = "peak 123 above"
+adeniyi = adeniyi.isprintable()
+print(adeniyi)
+
+
+
+
+
+
